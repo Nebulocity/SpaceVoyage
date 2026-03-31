890a2e6ccf69d03656119e0c8af53f493a9340a4
Ken <Nebulocity@users.noreply.github.com>
2026-03-30 17:03:04 -0700

    Fix import paths for TitleScene and GameScene

--- js/game.js
+++ js/game.js
@@ -19,8 +19,8 @@
 
 const Phaser = window.Phaser; // Phaser is loaded via <script> in index.html
 
-import TitleScene from './js/titleScene.js';
-import GameScene from './js/gameScene.js';
+import TitleScene from './titleScene.js';
+import GameScene from './gameScene.js';
 
 var config = {
     type: Phaser.AUTO,
